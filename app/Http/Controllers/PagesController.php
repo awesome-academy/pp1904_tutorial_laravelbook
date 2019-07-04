e510d2c35d114a764c8ce8cf10eeefa815e09626
quyenlv <quyen.luongvan13@gmail.com>
2019-07-04 14:52:22 +0700

    feat(webapp):Chapter 2

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index()
+    {
+    	return view('welcome');
+    }
+
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function about()
+    {
+    	return view('about');
+    }
+
+    public function contact()
+    {
+    	return view('contact');
+    }
+}
